3237819f736ffe6e341e00c755f498d48b99fe11
modicamartina-pixel <modica.martina@gmail.com>
2025-08-14 23:32:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANTE per GitHub Pages: base = nome repo
+// IMPORTANTE per GitHub Pages: base = nome del repo
 export default defineConfig({
   plugins: [react()],
-  base: '/FlipHelper/'
+  base: '/FlipHelper/',
 })
